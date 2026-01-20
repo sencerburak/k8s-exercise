ab56f760c51dd481d9b399ecdc61e608372d9783
Sencer Burak Okumus <sencerburakokumus@gmail.com>
2026-01-20 15:16:08 +0100

    ci: add python & kubernetes quality checks

--- apps/hello-app/src/app.py
+++ apps/hello-app/src/app.py
@@ -3,9 +3,11 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return jsonify(message=os.getenv("MESSAGE", "Hello"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
